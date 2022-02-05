7f54606a3dad81bafc93bdaf61b94bb41da7a37c
Ilja Buschujew <ilja.buschujew@evologics.de>
2022-02-05 13:10:25 +0100

    reading serial output and publishing to mqtt broker

--- RX/MQTT_publish.py
+++ RX/MQTT_publish.py
@@ -1,82 +1,73 @@
+from curses import baudrate
 import json
 import subprocess as sp
 from paho.mqtt import client as mqtt_client
 import random
 import time
+from datetime import datetime
+import sys
+import serial
 
-def load_config():
-    try:
-        conf_file = "./src/conf/config"
+broker = 'io.adafruit.com'
+port = 1883
+topics = ["b_ilja/feeds/temperature", "b_ilja/feeds/humidity", "b_ilja/feeds/alarm_temp", "b_ilja/feeds/alarm_hum"]
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+ADAFRUIT_IO_USERNAME = "b_ilja"
+ADAFRUIT_IO_KEY = "aio_FuoS98iogf9JapfSPc4ag1zH9J3o"
 
-        with open(conf_file) as config_file:
-            config_data = json.load(config_file)
+def read_data():
+    ser = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
+    time_old = 0
+    while True:
+        try:
+            data_str = ser.readline().decode('ascii')
+            if len(data_str) > 0:
+                data_dict = json.loads(data_str)
+                print(data_dict)
+                time_current = data_dict["time"]
+                if time_current > time_old:
+                    time_old = time_current
+                    temp = data_dict["temp"] 
+                    hum = data_dict["hum"] 
+                    dev_ID = data_dict["devID"]
+                    
+                    return [temp, hum], dev_ID
 
-            print("configuration file",conf_file, "read successful!")
-
-            return config_data
-
-    except Exception as e:
-        print(e)
-
-config_data = load_config()
-port = config_data['port']
-print(port)
-file = config_data['file']
-print(file)
-
-sp.run(["minicom", "-D", port, "-C", file])
-# sp.run(["timeout", "30", "minicom", "-D", port, "-C", file])
-
-# def load_data():
-#     try:
-#         sensorData_file="./redirect"
-
-#         with open (sensorData_file) as sdf:
-#             sdf = json.load
-        
-
-
-
-# broker = 'io.adafruit.com'
-# port = 1883
-# topic = "b_ilja/feeds/welcome-feed"
-# client_id = f'python-mqtt-{random.randint(0, 1000)}'
-# ADAFRUIT_IO_USERNAME = "b_ilja"
-# ADAFRUIT_IO_KEY = "aio_FuoS98iogf9JapfSPc4ag1zH9J3o"
-
-# def connect_mqtt():
-#     def on_connect(client, userdata, flags, rc):
-#         if rc == 0:
-#             print("Connected to MQTT Broker!")
-#         else:
-#             print("Failed to connect, return code %d\n", rc)
-#     # Set Connecting Client ID
-#     client = mqtt_client.Client(client_id)
-#     client.username_pw_set(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-#     client.on_connect = on_connect
-#     client.connect(broker, port)
-#     return client
-
-# def publish(client):
-#      while True:
-#          time.sleep(10)
-#          msg = f"messages: {}"
-#          result = client.publish(topic, msg)
-#          # result: [0, 1]
-#          status = result[0]
-#          if status == 0:
-#              print(f"Send `{msg}` to topic `{topic}`")
-#          else:
-#              print(f"Failed to send message to topic {topic}")
-
-# def run():
-#     client = connect_mqtt()
-#     client.loop_start()
-#     publish(client)
+        except Exception as e:
+            print("Error: " + str(e))
 
 
-# if __name__ == '__main__':
-#     run()
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client,topic, msg):
+    msg = f"{msg}"
+    result = client.publish(topic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+    
+if __name__ == '__main__':
+    client = connect_mqtt()
+    client.loop_start()
+    while True:
+        sensor_values, dev_ID = read_data()
+        for value, topic in zip(sensor_values, topics):
+            publish(client, topic, value)
+    
 
 
 
